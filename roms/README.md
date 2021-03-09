29a7fea7f3e9f1a588528883f706113e52cb0b75
STOPNOAnime <54212870+stopnoanime@users.noreply.github.com>
2021-03-09 21:41:38 +0100

    Update

--- roms/README.md
+++ roms/README.md
@@ -2,7 +2,11 @@
 
 This folder contains all CHIP-8 ROMs.
 
-Use the rom_to_bin.cpp utility to convert them into a single bin file. Then you need to write the hex txt file into a RAW sd card with no filesystem.
+Use the rom_to_hex.cpp utility to convert them into a single ASCII hex file. Then you need to write the hex values into a sd card with no filesystem.
 If you are on Windows you can use HxD to achive this.
+
+## rom_to_hex usage:
+
+`rom_to_hex.exe file1 <file2-64> `
 
 **NOTICE: None of the below games are owned nor made by me. Please let me know if you are one of the authors and would like to credited for you work!**
--- roms/rom_to_hex.cpp
+++ roms/rom_to_hex.cpp
@@ -6,8 +6,13 @@
 
 int main( int argc, char *argv[] )
 {
-    if (argc<2){
-        std::cout<<"No files specified";
+    if (argc < 2){
+        std::cout<<"No files specified"<<std::endl;
+        return -1;
+    }
+
+    if (argc > 65){
+        std::cout<<"Too many files specified. CHIP-8 can fit a maximum of 64 files."<<std::endl;
         return -1;
     }
 
@@ -16,22 +21,27 @@ int main( int argc, char *argv[] )
     std::vector<uint8_t> buffer;
     uint8_t tmp;
 
-    output_file.open("out.txt", std::ios::out);
+    output_file.open("hex_out.txt", std::ios::out);
 
     for(int i = 0; i < argc - 1; i++){
         input_file.open(argv[i+1], std::ios::in | std::ios::binary);
 
+        if(!input_file){
+            std::cout<<"Couldn't open file "<<argv[i+1]<<std::endl;
+            return -1;
+        }
+
         while ( input_file.read(reinterpret_cast<char*>(&tmp), sizeof(uint8_t)) ) buffer.push_back(tmp);
 
         if (buffer.size() > 4096 - 512) {
-            std::cout<<"File "<<argv[i+1]<<" is to larrge to fit on CHIP-8 RAM.";
+            std::cout<<"File "<<argv[i+1]<<" is to larrge to fit on CHIP-8 RAM."<<std::endl;
             return -1;
         }
+
         while (buffer.size() < 4096 - 512) buffer.push_back(0);
         
-        output_file <<std::hex <<std::setfill('0');
         for ( unsigned int i = 0; i < buffer.size(); i++ ) {
-            output_file<<std::hex <<std::setw(2) <<static_cast<int>(buffer[i]);
+            output_file<<std::setfill('0') <<std::hex <<std::setw(2) <<static_cast<int>(buffer[i]);
         };
 
         buffer.clear();
